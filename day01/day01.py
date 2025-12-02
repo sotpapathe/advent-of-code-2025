ad5409abb3d173b397fb8168070868808cd1c80a
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2025-12-02 18:55:03 +0200

    Solve day 1 puzzle 1

--- day01/day01.py
+++ day01/day01.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 import os
 
+n = 0
+dial = 50
 with open(os.path.dirname(os.path.realpath(__file__)) + '/input') as f:
-    pass # TODO: read file contents
+    for line in f:
+        dir = line[0]
+        dist = int(line[1:])
+        if dir == "L":
+            dial -= dist
+        else:
+            dial += dist
+        dial = dial % 100
+        if dial == 0:
+            n += 1
+print(n)
