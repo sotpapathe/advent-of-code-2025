2c69242eee822b5383166314dbd517bcef05e462
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2025-12-02 19:23:54 +0200

    Solve day 1 puzzle 2

--- day01/day01.py
+++ day01/day01.py
@@ -2,16 +2,25 @@
 import os
 
 n = 0
+n2 = 0
 dial = 50
-with open(os.path.dirname(os.path.realpath(__file__)) + '/input') as f:
+with open(os.path.dirname(os.path.realpath(__file__)) + '/input3') as f:
     for line in f:
         dir = line[0]
         dist = int(line[1:])
+        inc = dist // 100
+        n2 += inc
+        dist = dist % 100
+        at_zero = dial == 0
         if dir == "L":
             dial -= dist
         else:
             dial += dist
-        dial = dial % 100
+        dial_mod = dial % 100
+        if dial_mod != dial and not at_zero and dial_mod != 0:
+            n2 += 1
+        dial = dial_mod
         if dial == 0:
             n += 1
 print(n)
+print(n + n2)
