99df437685ce48e3a18296d95699ff15ab4ae48a
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2025-12-02 19:32:59 +0200

    Solve day 2 puzzle 1

--- day02/day02.py
+++ day02/day02.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 import os
 
+class Range:
+    def __init__(self, range):
+        r = range.split('-')
+        self.start = int(r[0])
+        self.end = int(r[1])
+
+    def __str__(self) -> str:
+        return '{}-{}'.format(self.start, self.end)
+
+n = 0
 with open(os.path.dirname(os.path.realpath(__file__)) + '/input') as f:
-    pass # TODO: read file contents
+    ranges = [Range(x) for x in f.readlines()[0].strip().split(',')]
+    for r in ranges:
+        for x in range(r.start, r.end + 1):
+            s = str(x)
+            if len(s) % 2 == 0:
+                if s[:len(s)//2] == s[len(s)//2:]:
+                    n += x
+print(n)
