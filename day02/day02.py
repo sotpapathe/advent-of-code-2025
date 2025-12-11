c05cfafd4c397278aaf1c31d6ecc61a885cc67c3
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2025-12-11 19:26:30 +0200

    Solve day 2 puzzle 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# (+){2,}
 import os
 
 class Range:
@@ -11,12 +12,25 @@ class Range:
         return '{}-{}'.format(self.start, self.end)
 
 n = 0
+n2 = 0
 with open(os.path.dirname(os.path.realpath(__file__)) + '/input') as f:
     ranges = [Range(x) for x in f.readlines()[0].strip().split(',')]
     for r in ranges:
         for x in range(r.start, r.end + 1):
             s = str(x)
+            # Part 1
             if len(s) % 2 == 0:
                 if s[:len(s)//2] == s[len(s)//2:]:
                     n += x
+            # Part 2
+            for seqlen in range(1, len(s)//2 + 1):
+                if len(s) % seqlen != 0:
+                    continue
+                seq = s[:seqlen]
+                r = len(s) // seqlen
+                if s == r * seq:
+                    n2 += x
+                    break
+
 print(n)
+print(n2)
