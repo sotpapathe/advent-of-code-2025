0a2d371bf80bf06fb79676ec7488eae933628f01
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2026-01-13 18:51:27 +0200

    Solve day 4

--- day04/day04.py
+++ day04/day04.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python3
 import os
 
+def accessible(rolls, x, y):
+    if not rolls[y][x]:
+        return False
+    n = 0
+    for yy in [-1, 0, 1]:
+        for xx in [-1, 0, 1]:
+            if xx == 0 and yy == 0:
+                continue
+            if x + xx < 0 or x + xx >= len(rolls[0]) or y + yy < 0 or y + yy >= len(rolls):
+                continue
+            if rolls[y + yy][x + xx]:
+                n += 1
+    return n < 4
+
+
 with open(os.path.dirname(os.path.realpath(__file__)) + '/input') as f:
-    pass # TODO: read file contents
+    rolls = [x.strip() for x in f.readlines()]
+rolls = [[True if col == '@' else False for col in row] for row in rolls]
+
+n_rolls = 0
+update = True
+while update:
+    update = False
+    removed = []
+    for y in range(len(rolls)):
+        for x in range(len(rolls[y])):
+            if accessible(rolls, x, y):
+                n_rolls += 1
+                removed.append((x, y))
+                update = True
+    for r in removed:
+        rolls[r[1]][r[0]] = False
+
+print(n_rolls)
